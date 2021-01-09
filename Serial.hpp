593e5872e86be5e036fa0ec5d5072c79521e4bc2
tabinohito <ugokazarukoto108nogotoshi@yahoo.co.jp>
2021-01-10 00:28:24 +0900

    renew Serial -> printf

--- Serial.hpp
+++ Serial.hpp
@@ -17,8 +17,9 @@ extern "C"{
 #include <array>
 #include <map>
 #include <string.h>
+#include <math.h>
 
-#define	_CR_CRLF		1	/* 1: Convert \n ==> \r\n in the output char */
+#define	XF_CRLF			1	/* 1: Convert \n ==> \r\n in the output char */
 #define	_USE_LONGLONG	0	/* 1: Enable long long integer in type "ll". */
 #define	_LONGLONG_t		long long	/* Platform dependent long long integer type */
 
@@ -64,7 +65,7 @@ class Serial{
 		{
 			va_list arp;
 			va_start(arp, fmt);
-			xvprintf(fmt, arp);
+			xvfprintf(fmt, arp);
 			va_end(arp);
 		};
 
@@ -77,12 +78,7 @@ class Serial{
 		void print(const int32_t fmt) { print(std::to_string(fmt)); };
 		void print(const std::string fmt) { printf(fmt.c_str()); };
 		void print(const char* fmt){ printf(fmt); };
-		void print(const float fmt,size_t precision = 2){ print(static_cast<double>(fmt),precision); };
-		void print(const double fmt,size_t precision = 2){
-			char temp[128] = {0};
-			ftoa(static_cast<double>(fmt),temp,precision);
-			printf("%s",temp);
-		};
+		void print(const float fmt){ printf("%f",fmt); };
 
 		void println(){ printf("\n"); };
 		void println(const char fmt){
@@ -103,17 +99,13 @@ class Serial{
 			printf(fmt);
 			printf("\n");
 		};
-		void println(const float fmt,size_t precision = 2){
-			println(static_cast<double>(fmt),precision);
-		};
-		void println(const double fmt,size_t precision = 2){
-			char temp[128] = {0};
-			ftoa(static_cast<double>(fmt),temp,precision);
-			printf("%s\n",temp);
+		void println(const float fmt){
+			printf("%f\n",fmt);
 		};
 
 	protected:
 		static constexpr size_t MAX_PRECISION = 10;
+		static constexpr size_t SZB_OUTPUT = 32;
 		const double rounders[10 + 1] =
 		{
 			0.5,				// 0
@@ -144,116 +136,165 @@ class Serial{
 		void init();
 
 		void xputc (char c){
-			if (_CR_CRLF && c == '\n') xputc('\r');		/* CR -> CRLF */
+			if (XF_CRLF && c == '\n') xputc('\r');		/* CR -> CRLF */
 
 			write((unsigned char)c);
 		};
-		void xputs (const char* str){
-			while (*str) {
-				xputc(*str++);
-			}
-		};
 
-		void xvprintf (
+		void xputc (
+			uint8_t  chr				/* Character to be output */
+		)
+		{
+			xfputc(chr);	/* Output it to the default output device */
+		}
+
+
+		void xfputc (			/* Put a character to the specified device */
+			uint8_t chr				/* Character to be output */
+		)
+		{
+			if (XF_CRLF && chr == '\n') xfputc('\r');	/* CR -> CRLF */
+			write(chr);		/* Write a character to the output device */
+		}
+
+
+
+		/*----------------------------------------------*/
+		/* Put a null-terminated string                 */
+		/*----------------------------------------------*/
+
+
+		void xvfprintf (
 			const char*	fmt,	/* Pointer to the format string */
 			va_list arp			/* Pointer to arguments */
-		){
-				unsigned int r, i, j, w, f;
-				char s[24], c, d, *p;
-			#if _USE_LONGLONG
-				_LONGLONG_t v;
-				unsigned _LONGLONG_t vs;
-			#else
-				long v;
-				unsigned long vs;
-			#endif
-				for (;;) {
-					c = *fmt++;					/* Get a format character */
-					if (!c) break;				/* End of format? */
-					if (c != '%') {				/* Pass it through if not a % sequense */
-						xputc(c); continue;
-					}
-					f = 0;						/* Clear flags */
-					c = *fmt++;					/* Get first char of the sequense */
-					if (c == '0') {				/* Flag: left '0' padded */
-						f = 1; c = *fmt++;
-					} else {
-						if (c == '-') {			/* Flag: left justified */
-							f = 2; c = *fmt++;
-						}
-					}
-					for (w = 0; c >= '0' && c <= '9'; c = *fmt++) {	/* Minimum width */
-						w = w * 10 + c - '0';
-					}
-					if (c == 'l' || c == 'L') {	/* Prefix: Size is long */
-						f |= 4; c = *fmt++;
-			#if _USE_LONGLONG
-						if (c == 'l' || c == 'L') {	/* Prefix: Size is long long */
-							f |= 8; c = *fmt++;
-						}
-			#endif
-					}
-					if (!c) break;				/* End of format? */
-					d = c;
-					if (d >= 'a') d -= 0x20;
-					switch (d) {				/* Type is... */
-					case 'S' :					/* String */
-						p = va_arg(arp, char*);
-						for (j = 0; p[j]; j++) ;
-						while (!(f & 2) && j++ < w) xputc(' ');
-						xputs(p);
-						while (j++ < w) xputc(' ');
-						continue;
-					case 'C' :					/* Character */
-						xputc((char)va_arg(arp, int)); continue;
-					case 'B' :					/* Binary */
-						r = 2; break;
-					case 'O' :					/* Octal */
-						r = 8; break;
-					case 'D' :					/* Signed decimal */
-					case 'U' :					/* Unsigned decimal */
-						r = 10; break;
-					case 'X' :					/* Hexdecimal */
-						r = 16; break;
-					default:					/* Unknown type (passthrough) */
-						xputc(c); continue;
-					}
+		)
+		{
+			unsigned int r, i, j, w, f;
+			int n, prec;
+			char str[SZB_OUTPUT], c, d, *p, pad;
+		#if XF_USE_LLI
+			XF_LLI_t v;
+			unsigned XF_LLI_t vs;
+		#else
+			long v;
+			unsigned long vs;
+		#endif
 
-					/* Get an argument and put it in numeral */
-			#if _USE_LONGLONG
-					if (f & 8) {	/* long long argument? */
-						v = va_arg(arp, _LONGLONG_t);
+			for (;;) {
+				c = *fmt++;					/* Get a format character */
+				if (!c) break;				/* End of format? */
+				if (c != '%') {				/* Pass it through if not a % sequense */
+					xfputc(c); continue;
+				}
+				f = w = 0;			 		/* Clear parms */
+				pad = ' '; prec = -1;
+				c = *fmt++;					/* Get first char of the sequense */
+				if (c == '0') {				/* Flag: left '0' padded */
+					pad = '0'; c = *fmt++;
+				} else {
+					if (c == '-') {			/* Flag: left justified */
+						f = 2; c = *fmt++;
+					}
+				}
+				if (c == '*') {				/* Minimum width from an argument */
+					n = va_arg(arp, int);
+					if (n < 0) {			/* Flag: left justified */
+						n = 0 - n; f = 2;
+					}
+					w = n; c = *fmt++;
+				} else {
+					while (c >= '0' && c <= '9') {	/* Minimum width */
+						w = w * 10 + c - '0';
+						c = *fmt++;
+					}
+				}
+				if (c == '.') {				/* Precision */
+					c = *fmt++;
+					if (c == '*') {				/* Precision from an argument */
+						prec = va_arg(arp, int);
+						c = *fmt++;
 					} else {
-						if (f & 4) {	/* long argument? */
-							v = (d == 'D') ? (long)va_arg(arp, long) : (long)va_arg(arp, unsigned long);
-						} else {		/* int/short/char argument */
-							v = (d == 'D') ? (long)va_arg(arp, int) : (long)va_arg(arp, unsigned int);
+						prec = 0;
+						while (c >= '0' && c <= '9') {
+							prec = prec * 10 + c - '0';
+							c = *fmt++;
 						}
 					}
-			#else
-					if (f & 4) {	/* long argument? */
-						v = va_arg(arp, long);
-					} else {		/* int/short/char argument */
-						v = (d == 'D') ? (long)va_arg(arp, int) : (long)va_arg(arp, unsigned int);
+				}
+				if (c == 'l') {		/* Prefix: Size is long */
+					f |= 4; c = *fmt++;
+		#if XF_USE_LLI
+					if (c == 'l') {	/* Prefix: Size is long long */
+						f |= 8; c = *fmt++;
 					}
-			#endif
-					if (d == 'D' && v < 0) {	/* Negative value? */
-						v = 0 - v; f |= 16;
-					}
-					i = 0; vs = v;
-					do {
-						d = (char)(vs % r); vs /= r;
-						if (d > 9) d += (c == 'x') ? 0x27 : 0x07;
-						s[i++] = d + '0';
-					} while (vs != 0 && i < sizeof s);
-					if (f & 16) s[i++] = '-';
-					j = i; d = (f & 1) ? '0' : ' ';
-					while (!(f & 2) && j++ < w) xputc(d);
-					do xputc(s[--i]); while (i != 0);
-					while (j++ < w) xputc(' ');
+		#endif
+				}
+				if (!c) break;				/* End of format? */
+				switch (c) {				/* Type is... */
+				case 'b':					/* Unsigned binary */
+					r = 2; break;
+				case 'o':					/* Unsigned octal */
+					r = 8; break;
+				case 'd':					/* Signed decimal */
+				case 'u':					/* Unsigned decimal */
+					r = 10; break;
+				case 'x':					/* Hexdecimal (abc) */
+				case 'X':					/* Hexdecimal (ABC) */
+					r = 16; break;
+				case 'c':					/* A character */
+					xfputc((char)va_arg(arp, int)); continue;
+				case 's':					/* String */
+					p = va_arg(arp, char*);
+					for (j = strlen(p); !(f & 2) && j < w; j++) xfputc(pad);	/* Left pads */
+					while (*p && prec--) xfputc(*p++);/* String */
+					while (j++ < w) xfputc(' ');		/* Right pads */
+					continue;
+				case 'f':					/* Float (decimal) */
+				case 'e':					/* Float (e) */
+				case 'E':					/* Float (E) */
+					ftoa(p = str, va_arg(arp, double), prec, c);	/* Make fp string */
+					for (j = strlen(p); !(f & 2) && j < w; j++) xfputc(pad);	/* Left pads */
+					do xfputc(*p++); while (*p);	/* Value */
+					while (j++ < w) xfputc(' ');	/* Right pads */
+					continue;
+				default:					/* Unknown type (passthrough) */
+					xfputc(c); continue;
 				}
 
-		};
+				/* Get an argument and put it in numeral */
+		#if XF_USE_LLI
+				if (f & 8) {	/* long long argument? */
+					v = va_arg(arp, XF_LLI_t);
+				} else {
+					if (f & 4) {	/* long argument? */
+						v = (c == 'd') ? (XF_LLI_t)va_arg(arp, long) : (XF_LLI_t)va_arg(arp, unsigned long);
+					} else {		/* int/short/char argument */
+						v = (c == 'd') ? (XF_LLI_t)va_arg(arp, int) : (XF_LLI_t)va_arg(arp, unsigned int);
+					}
+				}
+		#else
+				if (f & 4) {	/* long argument? */
+					v = va_arg(arp, long);
+				} else {		/* int/short/char argument */
+					v = (c == 'd') ? (long)va_arg(arp, int) : (long)va_arg(arp, unsigned int);
+				}
+		#endif
+				if (c == 'd' && v < 0) {	/* Negative value? */
+					v = 0 - v; f |= 1;
+				}
+				i = 0; vs = v;
+				do {	/* Make value string */
+					d = (char)(vs % r); vs /= r;
+					if (d > 9) d += (c == 'x') ? 0x27 : 0x07;
+					str[i++] = d + '0';
+				} while (vs != 0 && i < sizeof str);
+				if (f & 1) str[i++] = '-';				/* Sign */
+				for (j = i; !(f & 2) && j < w; j++) xfputc(
+						pad);	/* Left pads */
+				do xfputc(str[--i]); while (i != 0);	/* Value */
+				while (j++ < w) xfputc(' ');			/* Right pads */
+			}
+		}
 
 		char* ftoa(double f, char * buf,size_t precision)
 		{
@@ -341,6 +382,116 @@ class Serial{
 			*ptr = 0;
 
 			return buf;
+		};
+
+		int ilog10 (double n)	/* Calculate log10(n) in integer output */
+		{
+			int rv = 0;
+
+			while (n >= 10) {	/* Decimate digit in right shift */
+				if (n >= 100000) {
+					n /= 100000; rv += 5;
+				} else {
+					n /= 10; rv++;
+				}
+			}
+			while (n < 1) {		/* Decimate digit in left shift */
+				if (n < 0.00001) {
+					n *= 100000; rv -= 5;
+				} else {
+					n *= 10; rv--;
+				}
+			}
+			return rv;
+		};
+
+
+		double i10x (int n)	/* Calculate 10^n */
+		{
+			double rv = 1;
+
+			while (n > 0) {		/* Left shift */
+				if (n >= 5) {
+					rv *= 100000; n -= 5;
+				} else {
+					rv *= 10; n--;
+				}
+			}
+			while (n < 0) {		/* Right shift */
+				if (n <= -5) {
+					rv /= 100000; n += 5;
+				} else {
+					rv /= 10; n++;
+				}
+			}
+			return rv;
+		};
+
+
+		void ftoa (
+			char* buf,	/* Buffer to output the generated string */
+			double val,	/* Real number to output */
+			int prec,	/* Number of fractinal digits */
+			char fmt	/* Notation */
+		)
+		{
+			int d;
+			int e = 0, m = 0;
+			double w;
+			const char *er = 0;
+
+
+			if (isnan(val)) {			/* Not a number? */
+				er = "NaN";
+			} else {
+				if (prec < 0) prec = 6;	/* Default precision (6 fractional digits) */
+				if (val < 0) {			/* Nagative? */
+					val = -val; *buf++ = '-';
+				}
+				if (isinf(val)) {		/* Infinite? */
+					er = "INF";
+				} else {
+					if (fmt != 'f') {	/* E notation? */
+						if (val != 0) {		/* Not true zero? */
+							val += i10x(ilog10(val) - prec) / 2;	/* Round */
+							e = ilog10(val);
+							if (e > 99 || prec + 7 > SZB_OUTPUT) {
+								er = "OV";
+							} else {
+								if (e < -99) e = -99;
+								val /= i10x(e);	/* Normalize */
+							}
+						}
+					} else {			/* Decimal notation */
+						val += i10x(-prec) / 2;	/* Round */
+						m = ilog10(val);
+						if (m < 0) m = 0;
+						if (m + prec + 3 > SZB_OUTPUT) er = "OV";
+					}
+				}
+				if (!er) {	/* Not error condition */
+					do {				/* Put decimal number */
+						w = i10x(m);				/* Snip the highest digit d */
+						d = val / w; val -= d * w;
+						if (m == -1) *buf++ = '.';	/* Into fractional part? */
+						*buf++ = '0' + d;			/* Put the digit */
+					} while (--m >= -prec);			/* Output all digits specified by prec */
+					if (fmt != 'f') {	/* Put exponent if needed */
+						*buf++ = fmt;
+						if (e < 0) {
+							e = -e; *buf++ = '-';
+						} else {
+							*buf++ = '+';
+						}
+						*buf++ = '0' + e / 10;
+						*buf++ = '0' + e % 10;
+					}
+				}
+			}
+			if (er) {	/* Error condition? */
+				do *buf++ = *er++; while (*er);
+			}
+			*buf = 0;	/* Term */
 		};
 
 };
